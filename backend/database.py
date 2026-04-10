fe8638c57135c99e211656664f1fbe50df663306
sridharanraja2008-prog <sridharanraja2008@gmail.com>
2026-04-10 12:37:45 +0530

    Delete backend directory

--- backend/database.py
+++
@@ -1,8 +0,0 @@
-import os
-from motor.motor_asyncio import AsyncIOMotorClient
-from dotenv import load_dotenv
-
-load_dotenv()
-
-client = AsyncIOMotorClient(os.getenv("MONGO_URI", "mongodb://localhost:27017"))
-db     = client["stock_tracker"]
