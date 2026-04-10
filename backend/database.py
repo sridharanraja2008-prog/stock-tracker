c086582e14b8314155bb53fb2c4d8ffc6c78ed03
sridharanraja2008-prog <sridharanraja2008@gmail.com>
2026-04-10 12:32:11 +0530

    Add files via upload

---
+++ backend/database.py
@@ -0,0 +1,8 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = AsyncIOMotorClient(os.getenv("MONGO_URI", "mongodb://localhost:27017"))
+db     = client["stock_tracker"]
